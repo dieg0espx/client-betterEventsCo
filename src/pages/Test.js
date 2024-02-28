732fbf26930c647ee6f0bb2077976b6b960146a2
dieg0espx <espinosa9mx@gmail.com>
2024-02-28 02:08:55 -0800

    .

--- src/pages/Test.js
+++ src/pages/Test.js
@@ -1,29 +1,27 @@
-import React, { useEffect } from 'react'
+import React, { useEffect } from 'react';
+import axios from 'axios';
 
 function Test() {
+  const APIKEY = 'AIzaSyBJLc7G1TkrBTRq3cge-TsYgNpEvDz3pyM';
 
-    const APIKEY = 'AIzaSyBJLc7G1TkrBTRq3cge-TsYgNpEvDz3pyM'
+  async function calculateDistance() {
+    const deliveryAddress = '610 Granville Street, Vancouver';
+    const vendorAddress = '488 Helmcken Street, Vancouver';
+    const apiURL = `https://maps.googleapis.com/maps/api/distancematrix/json?units=imperial&origins=${vendorAddress}&destination=${deliveryAddress}&key=${APIKEY}`;
 
-    async function calculateDistance(){
-        const deliveryAddress = '610 Granville Street, Vancouver'
-        const vendorAddress =  '488 Helmcken Street, Vancouver'
-        const apiURL = `https://maps.googleapis.com/maps/api/distancematrix/json?units=imperial&origins=${vendorAddress}&destination=${deliveryAddress}&key=${APIKEY}`
-
-        console.log(apiURL);
-        await fetch(apiURL)
-          .then((response) => response.json())
-          .then((response) => console.log(response))
-          .catch((error) => console.error('Error fetching data:', error));
+    try {
+      const response = await axios.get(apiURL);
+      console.log(response.data);
+    } catch (error) {
+      console.error('Error fetching data:', error);
     }
+  }
 
-    useEffect(()=>{
-        calculateDistance()
-    },[])
+  useEffect(() => {
+    calculateDistance();
+  }, []);
 
-
-  return (
-    <div>Test</div>
-  )
+  return <div>Test</div>;
 }
 
-export default Test
+export default Test;
