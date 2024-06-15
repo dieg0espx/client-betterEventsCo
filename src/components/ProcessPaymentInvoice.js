e4ccfa3cd389fe6fe7683ef98b504c2c0a1a356f
dieg0espx <espinosa9mx@gmail.com>
2024-06-15 01:38:10 -0700

    .

--- src/components/ProcessPaymentInvoice.js
+++ src/components/ProcessPaymentInvoice.js
@@ -58,7 +58,7 @@ function ProcessPayment(props) {
                 id,
                 description: "INFLATABLE BOOKING", 
                 // amount: Math.floor(props.total*100),
-                amount: Math.floor(0),
+                amount: Math.floor(10),
               })
               if (response.data.success) {
                   updatePaid()
